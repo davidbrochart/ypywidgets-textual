4395365b6e85fa3732c51df905b9b1825650a810
David Brochart <david.brochart@gmail.com>
2023-10-18 15:44:06 +0200

    Add widget model on_change(callback)

--- ypywidgets_textual/plotext.py
+++ ypywidgets_textual/plotext.py
@@ -1,4 +1,6 @@
-from ypywidgets import Widget, reactive
+from ypywidgets import reactive
+
+from .widget import Widget
 
 
 class Plotext(Widget):
@@ -11,10 +13,6 @@ class Plotext(Widget):
 
     def __init__(self, primary: bool = True) -> None:
         super().__init__(primary=primary)
-        self._clear_data_change_callbacks = []
-        self._scatter_change_callbacks = []
-        self._plot_change_callbacks = []
-        self._title_change_callbacks = []
 
     def clear_data(self) -> None:
         self._clear_data = not self._clear_data
@@ -29,31 +27,3 @@ class Plotext(Widget):
 
     def title(self, value: str) -> None:
         self._title = value
-
-    def _on__clear_data_change(self, cb):
-        self._clear_data_change_callbacks.append(cb)
-
-    def _on__scatter_change(self, cb):
-        self._scatter_change_callbacks.append(cb)
-
-    def _on__plot_change(self, cb):
-        self._plot_change_callbacks.append(cb)
-
-    def _on__title_change(self, cb):
-        self._title_change_callbacks.append(cb)
-
-    def watch__clear_data(self, value: bool) -> None:
-        for cb in self._clear_data_change_callbacks:
-            cb(value)
-
-    def watch__scatter(self, value) -> None:
-        for cb in self._scatter_change_callbacks:
-            cb(value)
-
-    def watch__plot(self, value) -> None:
-        for cb in self._plot_change_callbacks:
-            cb(value)
-
-    def watch__title(self, value: str) -> None:
-        for cb in self._title_change_callbacks:
-            cb(value)
